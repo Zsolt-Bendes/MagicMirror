288f0a5f7b5a43fec249c870eabdde379d74b084
Zsolt Bendes <zsolt.bendes@gmail.com>
2019-11-19 18:41:29 +0100

    Refactored WeatherWorker to have list of cities, that will be queryed for each weachter request

--- MagicMirrorWorker/Workers/WeatherWorker.cs
+++ MagicMirrorWorker/Workers/WeatherWorker.cs
@@ -12,64 +12,74 @@ using System.Threading.Tasks;
 
 namespace MagicMirrorWorker.Workers
 {
-    public class WeatherWorker : BackgroundService
-    {
-        private readonly ILogger<WeatherWorker> _logger;
-        private readonly IConfiguration _configuration;
-        private readonly IHttpClientFactory _httpClientFactory;
-        private readonly IMemoryCache _cache;
+	public class WeatherWorker : BackgroundService
+	{
+		private static readonly string[] _cities = new string[] { "Győr", "Vienna" };
+		private readonly Task<OpenWeather>[] _weatherTasks = new Task<OpenWeather>[_cities.Length];
 
-        public WeatherWorker(
-            ILogger<WeatherWorker> logger,
-            IConfiguration configuration,
-            IHttpClientFactory httpClientFactory,
-            IMemoryCache cache)
-        {
-            _logger = logger;
-            _configuration = configuration;
-            _httpClientFactory = httpClientFactory;
-            _cache = cache;
-        }
+		private readonly ILogger<WeatherWorker> _logger;
+		private readonly IConfiguration _configuration;
+		private readonly IHttpClientFactory _httpClientFactory;
+		private readonly IMemoryCache _cache;
 
-        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                try
-                {
-                    var getWeatherInGyor = GetWeatherAsync("Győr");
-                    var getWeatherInVienna = GetWeatherAsync("Vienna");
+		public WeatherWorker(
+			ILogger<WeatherWorker> logger,
+			IConfiguration configuration,
+			IHttpClientFactory httpClientFactory,
+			IMemoryCache cache)
+		{
+			_logger = logger;
+			_configuration = configuration;
+			_httpClientFactory = httpClientFactory;
+			_cache = cache;
+		}
 
-                    _cache.Set(Constants.LATEST_FORECAST_CACHE_KEY, new WeatherResults
-                    {
-                        Weathers = new OpenWeather[]
-                        {
-                        await getWeatherInGyor,
-                        await getWeatherInVienna
-                        }
-                    });
+		protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			while (!stoppingToken.IsCancellationRequested)
+			{
+				try
+				{
+					for (int i = 0; i < _cities.Length; i++)
+					{
+						_weatherTasks[i] = GetWeatherAsync(_cities[i]);
+					}
 
-                    await Task.Delay(TimeSpan.FromMinutes(60));
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError($"Unexpected error fetching weather data: {ex.Data}");
-                } 
-            }
-        }
+					var responseContainer = new WeatherResults
+					{
+						Weathers = new OpenWeather[_cities.Length]
+					};
 
-        private async Task<OpenWeather> GetWeatherAsync(string cityName)
-        {
-            var client = _httpClientFactory.CreateClient(Constants.OPEN_WEATHER_CLIENT_NAME);
-            client.BaseAddress = new Uri(_configuration["OpenWeather:Url"]);
+					await Task.WhenAll(_weatherTasks);
 
-            var response = await client.GetAsync($"weather?q={cityName}&APPID={_configuration["OpenWeather:AppId"]}&units={_configuration["OpenWeather:Unit"]}&lang=hu");
-            if (response.IsSuccessStatusCode)
-            {
-                return await JsonSerializer.DeserializeAsync<OpenWeather>(await response.Content.ReadAsStreamAsync());
-            }
+					for (int i = 0; i < _weatherTasks.Length; i++)
+					{
+						responseContainer.Weathers[i] = _weatherTasks[i].Result;
+					}
 
-            throw new Exception("Unexpected error fetching weather data");
-        }
-    }
+					_cache.Set(Constants.LATEST_FORECAST_CACHE_KEY, responseContainer);
+
+					await Task.Delay(TimeSpan.FromMinutes(5));
+				}
+				catch (Exception ex)
+				{
+					_logger.LogError($"Unexpected error fetching weather data: {ex.Data}");
+				}
+			}
+		}
+
+		private async Task<OpenWeather> GetWeatherAsync(string cityName)
+		{
+			var client = _httpClientFactory.CreateClient(Constants.OPEN_WEATHER_CLIENT_NAME);
+			client.BaseAddress = new Uri(_configuration["OpenWeather:Url"]);
+
+			var response = await client.GetAsync($"weather?q={cityName}&APPID={_configuration["OpenWeather:AppId"]}&units={_configuration["OpenWeather:Unit"]}&lang=hu");
+			if (response.IsSuccessStatusCode)
+			{
+				return await JsonSerializer.DeserializeAsync<OpenWeather>(await response.Content.ReadAsStreamAsync());
+			}
+
+			throw new Exception("Unexpected error fetching weather data");
+		}
+	}
 }
